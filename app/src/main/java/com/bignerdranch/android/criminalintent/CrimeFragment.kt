5fee0aba4bd4595fb775b1697926b87916b4b071
Evgeny R <ryderi700@gmail.com>
2023-05-19 08:11:41 +0300

    RecycleView

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.kt
@@ -6,6 +6,8 @@ import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 
@@ -14,6 +16,8 @@ class CrimeFragment: Fragment() {
 
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
+    private lateinit var dateButton: Button
+    private lateinit var solvedCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +30,13 @@ class CrimeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
        val view = inflater.inflate(R.layout.fragment_crime, container, false)
-        titleField = view.findViewById(R.id.crime_title)
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as Button
+        solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
+        dateButton.apply {
+            text = crime.date.toString()
+            isEnabled = false
+        }
        return view
     }
 
@@ -47,5 +57,10 @@ class CrimeFragment: Fragment() {
         }
 
         titleField.addTextChangedListener(titleWatcher)
+        solvedCheckBox.apply {
+            setOnCheckedChangeListener {_, isChecked ->
+                crime.isSolved = isChecked
+            }
+        }
     }
 }
--- app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -3,9 +3,19 @@ package com.bignerdranch.android.criminalintent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_ссontainer)
+        if (currentFragment == null) {
+            val fragment = CrimeListFragment.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_ссontainer, fragment)
+                .commit()
+        }
     }
 }
